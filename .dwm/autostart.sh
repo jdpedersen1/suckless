cd226afed54bb905f405a685972aa85d205d02fb
Jacob Pedersen <jdpedersen1@yahoo.com>
2021-10-19 21:40:34 -0500

    adding autostart

---
+++ .dwm/autostart.sh
@@ -0,0 +1,12 @@
+sxhkd &
+picom --experimental-backend &
+#picom -b &
+dwmblocks &
+xwallpaper --center ~/Pictures/wallpaper/j@l.png
+xfce4-power-manager &
+/usr/bin/dunst &
+mpdris2.desktop &
+/usr/bin/emacs --daemon &
+#nitrogen --random --set-scaled &
+conky -c ~/.config/conky/dwm.conkyrc &
+mailspring -b &
